afdda9f91bf51a5a59b82330187a2fe1eb0c91e2
zzkevin <realzzkevin@gmail.com>
2021-05-09 03:38:38 -0400

    all done

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -4,10 +4,10 @@ function renderManagerCard(manager){
     
     if(manager.getRole()==="Manager"){
         let card =`
-                    <li class='card'>
-                        <div class = 'card-header text-center'>
-                            <h2>${manager.getName()}</h2>
-                            <h3><span class ='fas fa-user-tie'></span>${manager.getRole()}</h3>
+                    <li class='card' style="max-width: 18rem; min-width: 18rem;">
+                        <div class = 'card-header text-center bg-info text-light'>
+                            <h2 class='card-title'>${manager.getName()}</h2>
+                            <h3 class ='card-text' ><span class ='fas fa-user-tie'></span>${manager.getRole()}</h3>
                         </div>
                         <div class='card-body'>
                             <ul class='list-group'>
@@ -27,16 +27,16 @@ function renderManagerCard(manager){
 function renderEngineerCard(engineer){
     if(engineer.getRole()==='Engineer'){
         let card = `
-                    <li class='card'>
-                        <div class = 'card-header text-center'>
-                            <h2>${engineer.getName()}</h2>
-                            <h3><span class ='fas fa-user-cog'></span>${engineer.getRole()}</h3>
+                    <li class='card' style="max-width: 18rem; min-width: 18rem;">
+                        <div class = 'card-header text-center bg-info text-light'>
+                            <h2 class='card-title'>${engineer.getName()}</h2>
+                            <h3 class ='card-text'><span class ='fas fa-user-cog'></span>${engineer.getRole()}</h3>
                         </div>
                         <div class='card-body'>
                             <ul class='list-group'>
                                 <li class='list-group-item fas fa-id-badge'> ID: ${engineer.getId()}</li>
                                 <li class='list-group-item fas fa-at'> Email: <a href='mailto:${engineer.getEmail()}'> ${engineer.getEmail()}</a>  </li>
-                                <li class='list-group-item fab fa-github'> <a href='https://github.com/${engineer.getGithub()}'>: ${engineer.getGithub()} </a></li>
+                                <li class='list-group-item fab fa-github'> github: <a href='https://github.com/${engineer.getGithub()}'>${engineer.getGithub()} </a></li>
                             </ul>
                         </div>
                     </li>\n`;
@@ -50,10 +50,10 @@ function renderEngineerCard(engineer){
 function renderInternCard(intern){
     if (intern.getRole()==='Intern'){
         let card =`
-                    <li class='card'>
-                        <div class = 'card-header text-center'>
-                            <h2>${intern.getName()}</h2>
-                            <h3><span class='fas fa-user-graduate'></span>${intern.getRole()}</h3>
+                    <li class='card' style="max-width: 18rem; min-width: 18rem;">
+                        <div class = 'card-header text-center bg-info text-light'>
+                            <h2 class='card-title'>${intern.getName()}</h2>
+                            <h3 class ='card-text'><span class='fas fa-user-graduate'></span>${intern.getRole()}</h3>
                         </div>
                         <div class='card-body'>
                             <ul class='list-group'>
@@ -102,14 +102,14 @@ function generateHTML(team){
                         <title>My Team</title>
                     </head>
                     <body>
-                        <header class="jumbotron jumbotron-fluid">
+                        <header class="jumbotron jumbotron-fluid text-light bg-primary">
                             <div class='container text-center'>
                                 <h1>My Team</h1>
                             </div>
                         </header>
                         <main class='container-fluid'>
-                            <section class='row'>
-                                <ul class ="card-deck">
+                            <section class='row justify-content-center'>
+                                <ul class ="card-deck w-75">
                                     ${list}
                                 </ul>
                             </section>
